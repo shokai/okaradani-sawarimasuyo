a529036f17f859113582c752db46772450b8e114
Sho Hashimoto <hashimoto@shokai.org>
2012-10-27 01:08:01 +0900

    say error if tweet is empty

--- okaradani-sawarimasuyo.rb
+++ okaradani-sawarimasuyo.rb
@@ -9,7 +9,9 @@ threshold = 20
 client = Tw::Client.new
 client.auth
 
-tweets = client.user_timeline('shokai_log').sort{|a,b|
+tweets = client.user_timeline('shokai_log').select{|m|
+  Time.now - m.time < 60*60
+}.sort{|a,b|
   b.time <=> a.time
 }.map{|m|
   JSON.parse m.text rescue nil
@@ -19,12 +21,19 @@ tweets = client.user_timeline('shokai_log').sort{|a,b|
 
 p tweets
 
+if tweets.empty?
+  STDERR.puts '温度がわかりません、エラーっぽいです'
+  system 'saykana おんどがわかりません、えらーっぽいです'
+  exit 1
+end
+
 temp = tweets[0]["気温"]
 puts "現在の気温 #{temp}度"
 system "saykana 'げんざいのきおん'"
 sleep 0.5
 system "saykana '#{temp.to_i}ど'"
-if temp < threshold
-  sleep 0.5
-  system 'saykana おからだにさわりますよ'
-end
+exit unless temp < threshold
+
+sleep 0.5
+system 'saykana おからだにさわりますよ'
+
