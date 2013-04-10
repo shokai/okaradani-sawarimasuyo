81c4372ab65e0310eb41da5ebf219ffbe838a647
Sho Hashimoto <hashimoto@shokai.org>
2013-04-11 02:25:44 +0900

    use args_parser, set say command path

--- okaradani-sawarimasuyo.rb
+++ okaradani-sawarimasuyo.rb
@@ -4,12 +4,24 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 
-threshold = 20
+parser = ArgsParser.parse ARGV do
+  arg :threshold, 'temperature threshold', :default => 20
+  arg :twitter_user, 'twitter username', :alias => :twitter
+  arg :say_path, 'say command path', :alias => :say, :default => `which say`.strip
+  arg :help, 'show help', :alias => :h
+end
+
+if parser.has_option? :help or !parser.has_param? :twitter_user
+  STDERR.puts parser.help
+  STDERR.puts
+  STDERR.puts "e.g.  #{$0} --twitter shokai_log --threshold 20 --say /usr/local/bin/saykana"
+  exit 1
+end
 
 client = Tw::Client.new
 client.auth
 
-tweets = client.user_timeline('shokai_log').select{|m|
+tweets = client.user_timeline(parser[:twitter_user]).select{|m|
   Time.now - m.time < 60*60
 }.sort{|a,b|
   b.time <=> a.time
@@ -23,17 +35,17 @@ p tweets
 
 if tweets.empty?
   STDERR.puts '温度がわかりません、エラーっぽいです'
-  system 'saykana おんどがわかりません、えらーっぽいです'
+  system "#{parser[:say_path]} 'おんどがわかりません、えらーっぽいです'"
   exit 1
 end
 
 temp = tweets[0]["気温"]
 puts "現在の気温 #{temp}度"
-system "saykana 'げんざいのきおん'"
+system "#{parser[:say_path]} 'げんざいのきおん'"
 sleep 0.5
-system "saykana '#{temp.to_i}ど'"
-exit unless temp < threshold
+system "#{parser[:say_path]} '#{temp.to_i}ど'"
+exit unless temp < parser[:threshold]
 
 sleep 0.5
-system 'saykana おからだにさわりますよ'
+system "#{parser[:say_path]} 'おからだにさわりますよ'"
 
